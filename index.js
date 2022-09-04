757fa29bf6e00a2963eb8014244f9c3cfb441d44
DannyMX29 <bran.dan.her.gar@gmail.com>
2022-09-04 15:47:31 -0500

    munu automatico y diseño responsivo

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+const menuHamIcon = document.querySelector("#menu");
+const mobielMenu = document.querySelector("#navbar");
+
+
+menuHamIcon.addEventListener("click", toggleMobilepMenu);
+
+function toggleMobilepMenu()
+{
+        mobielMenu.classList.remove('collapse');
+        setTimeout(cerrar, 3000);
+}
+
+function cerrar(){
+    mobielMenu.classList.add('collapse');
+}
+
+
+
+
 $(function (){
     $('[data-toggle="tooltip"]') .tooltip()
 })
